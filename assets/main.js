8fbe2ef59c577a420f03962e6aa274e9ce27f949
Chris Grafton <{ID}+{username}@users.noreply.github.com>
2023-01-15 14:55:03 -0800

    screen show display during shows

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,18 @@
 ---
 ---
+
+var messages = ['Attitude Check!',
+    '<span class="text-dark">Buy a Bawdy Bag</span><br> Everything you need to <strong>throw, bloww, and glow</strong> at the Rocky Horror Picture Show',
+    "bawdycaste.com"
+];
+var counter = 0;
+var message = document.getElementById('message');
+var inst = setInterval(change,10000);
+
+function change() {
+    message.innerHTML = messages[counter];
+    counter++;
+    if (counter >= messages.length) {
+        counter = 0;
+    }
+}
