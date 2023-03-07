31bd062e42c4c2cb44ecdaef6116d041c25280f0
Chris Grafton <{ID}+{username}@users.noreply.github.com>
2023-03-07 00:05:23 -0800

    use bundle to update jekyll

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,15 @@
 ---
 ---
 
-var messages = ['Attitude Check!',
-    '<span class="text-dark">Buy a Bawdy Bag</span><br> Everything you need to <strong>throw, bloww, and glow</strong> at the Rocky Horror Picture Show',
-    "bawdycaste.com"
-];
+var messages = [
+    'Fuck You!',
+    'Buy a Bawdy Bag in the lobby!',
+    'bawdycaste.com'];
 var counter = 0;
 var message = document.getElementById('message');
-var inst = setInterval(change,10000);
+var inst = setInterval(change, 30000);
 
+message.innerHTML = "Attitude Check!";
 function change() {
     message.innerHTML = messages[counter];
     counter++;
