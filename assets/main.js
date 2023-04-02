63479a28a9a9e40b990c96b319ed3999eca41229
Chris Grafton <{ID}+{username}@users.noreply.github.com>
2023-04-01 18:58:28 -0700

    add lines suggested by lux

--- assets/main.js
+++ assets/main.js
@@ -9,6 +9,9 @@ var messages = [
     'Hormone Check?<br><p class="display-1 pt-5">Fuck Me!</p>',
     'Follow Us on Social Media',
     'Buy a Large Popcorn!',
+    'We Want YOU to join the BAWDY CASTE',
+    'Thank you for wearing a mask!',
+    'Our All-Volunteer Cast will be Available for Photos & Accepting Donations After the Show',
     'Attitude Check?'
 ];
 var counter = 0;
