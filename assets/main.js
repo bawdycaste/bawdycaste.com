93a41151213bb043e08fefa2498fc22ebbe8dbc0
Chris Robert <3653760+crgrafton@users.noreply.github.com>
2024-02-26 23:05:23 -0800

    Refactor message display and update in main.js and balboa.html

--- assets/main.js
+++ assets/main.js
@@ -6,38 +6,29 @@ const messages = [
     'Buy a Bawdy Bag',
     'bawdycaste.com',
     'Hormone Check?',
-    'Hormone Check?<br><p class="display-1 pt-5">Fuck Me!</p>',
+    'Hormone Check?<br><p class="pt-5">Fuck Me!</p>',
     'Follow Us on Social Media',
+    'Donate to Support the Show',
     'Buy a Large Popcorn!',
-    'We Want YOU to join the BAWDY CASTE',
+    'Join our Cast!',
     'Thank you for wearing a mask!',
-    'Cast Available for Photos & Accepting Donations After the Show',
+    'Cast Available for Photos After the Show',
     'All Volunteer Cast - We do not get Paid',
-    'All Volunteer Cast - We do not get Paid<br><br><span class="pt-5">wait what?!?! i am not getting paid?</span>',
+    '<span class="pt-5">wait what?!?! im not getting paid??</span>',
+    ' ',
     ' ',
     ' ',
     'Attitude Check?'
 ];
 var counter = 0;
-const message = document.getElementById('message');
+var message = document.getElementById('message');
 var inst = setInterval(change, 10000);
 
+message.innerHTML = "Attitude Check?" ;
 function change() {
-    const messageIndex = messages.indexOf(paragraphElement.textContent) + 1;
-    if (messageIndex >= messages.length) {messageIndex = 0;}
-    paragraphElement.textContent = messages[messageIndex];
-    // message.innerHTML = messages[counter];
-    // counter++;
-    // if (counter >= messages.length) {
-        // counter = 0;
-    // }   
+    message.innerHTML = messages[counter];
+    counter++;
+    if (counter >= messages.length) {
+        counter = 0;
+    }
 }
-change();
-
-setInterval(updateText, 60000);
-
-// message.innerHTML = "Attitude Check?" ;
-
-// javascript to change a div with id message to a list of strings every second"
-
-
